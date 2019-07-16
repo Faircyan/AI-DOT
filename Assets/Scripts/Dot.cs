5d6d882f28d7169abe5ebdbfccf593ebf5e5e899
faircyan <09fatihhamza@gmail.com>
2019-07-16 10:41:39 +0300

    upload all files

---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+
+    public Vector3 position;
+    public Vector3 velocity;
+    public Vector3 acc;
+    public Vector3[] directions;
+    public bool isStart = false;
+    public bool isDead = false;
+    public float fitness = 0;
+    public bool reachedGoal = false;
+    public bool isBest = false;
+    public int initialStep = 0;
+    public int finalStep = 1000;
+
+    // Use this for initialization
+    void Start () {
+        createDirections();
+        position = new Vector3(0, -4f, 0);
+        transform.position = position;
+    }
+	
+    void createDirections()
+    {
+        directions = new Vector3[1000];
+        FillDirections();
+    }
+
+    void FillDirections()
+    {
+        for (int i = 0; i < directions.Length; i++)
+        {
+            float randomAngle = Random.Range(-Mathf.PI, Mathf.PI);
+            directions[i] = new Vector3(Mathf.Cos(randomAngle) / 300, Mathf.Sin(randomAngle) / 300, 0);
+        }
+    }
+
+    public void StartAgain()
+    {
+        position = new Vector3(0, -4f, 0);
+        isDead = false;
+        fitness = 0;
+        reachedGoal = false;
+        isBest = false;
+        finalStep = 1000;
+        initialStep = 0;
+        isStart = true;
+        velocity = new Vector3(0, 0, 0);
+        transform.position = position;
+    }
+
+    public void Show()
+    {
+        transform.position = position;
+    }
+
+    public void Move()
+    {
+        if (directions.Length > initialStep)
+        {
+            acc = directions[initialStep];
+            initialStep++;
+        }
+        else
+            isDead = true;
+        velocity = new Vector3(velocity.x + acc.x, velocity.y + acc.y, 0);
+        position = new Vector3(position.x + velocity.x, position.y + velocity.y, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (transform.position.x < 1.1f && (transform.position.y < -0.9f && transform.position.y > -1.1f)
+           || transform.position.x > -0.8f && (transform.position.y < 1.6f && transform.position.y > 1.4f))
+            isDead = true;
+
+        if (isStart && !isDead)
+        {
+            Move();
+            if (transform.position.x <= -2.65 || transform.position.x >= 2.65 || transform.position.y <= -4.75 || transform.position.y >= 4.75)
+                isDead = true;
+            Show();
+        }
+        if (Mathf.Abs(transform.position.x - 0) <= 0.1f && Mathf.Abs(transform.position.y - 4.5f) <= 0.1f)
+        {
+            reachedGoal = true;
+            isDead = true;
+            finalStep = initialStep;
+        }
+        if (isDead)
+        {
+            isStart = false;
+            CalculateFitness();
+        }
+    }
+    public void CalculateFitness()
+    {
+
+       float distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - 0, 2) + Mathf.Pow(transform.position.y - 4.5f, 2));
+       fitness = 1 / (distance * distance);
+
+    }
+
+    public void newDirections(Vector3[] news)
+    {
+        int mutateRatio = 100;
+        for (int i = 0; i < news.Length; i++)
+        {
+            if (mutateRatio > Random.Range(0, 1000))
+            {
+                float randomAngle = Random.Range(-Mathf.PI, Mathf.PI);
+                directions[i] = new Vector3(Mathf.Cos(randomAngle) / 300, Mathf.Sin(randomAngle) / 300, 0);
+            }
+            else
+                directions[i] = news[i];
+        }
+    }
+
+}
---
+++ Assets/Scripts/Population.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Population : MonoBehaviour {
+
+    public GameObject[] dots;
+    public GameObject blackDot;
+    public int gen = 1;
+    public int bestDot = 0;
+    public int minStep = 1000;
+    public float totalFitness = 0;
+    public Text step;
+    public Text generation;
+
+
+	// Use this for initialization
+	void Start () {
+        dots = new GameObject[500];
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i] = Instantiate(blackDot, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+        StartMove();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        generation.text = "Gen:" + gen;
+        
+        if (isAllDotsDead())
+        {
+            FindBest();
+            step.text = "Step:" + dots[bestDot].GetComponent<Dot>().finalStep;
+            CreateNewGen();
+            calcTotalFitness();
+            StartAgain();
+            bestDot = 0;
+            gen++;
+        }
+        
+	}
+
+    void calcTotalFitness()
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            totalFitness += dots[i].GetComponent<Dot>().fitness;
+        }
+        totalFitness = 0;
+    }
+
+    void StartAgain()
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i].GetComponent<Dot>().StartAgain();
+        }
+
+    }
+    public void StartMove()
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i].GetComponent<Dot>().isStart = true;
+        }
+    }
+
+    private bool isAllDotsDead()
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            if (!dots[i].GetComponent<Dot>().isDead)
+                return false;
+        }
+        return true;
+    }
+
+    private void FindBest()
+    {
+        bool reached = false;
+        int temp = 0;
+        for (int i = 0; i < dots.Length; i++)
+        {
+            if (dots[i].GetComponent<Dot>().reachedGoal && dots[i].GetComponent<Dot>().finalStep <= dots[temp].GetComponent<Dot>().finalStep)
+            {
+                temp = i;
+                reached = true;
+            }
+        }
+        if (!reached)
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                if(dots[i].GetComponent<Dot>().fitness > dots[temp].GetComponent<Dot>().fitness)
+                {
+                    temp = i;
+                }
+            }
+        }
+        bestDot = temp;
+    }
+
+    void CreateNewGen()
+    {
+        Vector3[] bestDirects = dots[bestDot].GetComponent<Dot>().directions;
+
+        for (int i = 0; i < bestDirects.Length; i++)
+        {
+            dots[0].GetComponent<Dot>().directions[i] = bestDirects[i];
+        }
+
+
+        for (int i = 1; i < dots.Length; i++)
+        {
+
+            dots[i].GetComponent<Dot>().newDirections(bestDirects);
+        }
+
+    }
+
+}
